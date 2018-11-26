8e732df865f98a67d18088cd51d5f49561c7328e
KalidasM <kalidasm610@gmail.com>
2018-11-26 15:31:31 +0530

    Replace temp logger with cloud logging

--- Cloners.js
+++ Cloners.js
@@ -2,51 +2,52 @@ function cloneAttachmentsToFolder(threadMessage, attachments, folderId){
   attachments.forEach(function(attachment){
     var attachmentBlob = attachment.copyBlob();
     var folder = DriveApp.getFolderById(folderId);
-    
+
     var createdFile = folder.createFile(attachmentBlob);
-    createdFile.setDescription(threadMessage.getDate().toDateString());
+    createdFile.setDescription(threadMessage.getDate().toDateString() + "\n\n\n" + threadMessage.getBody());
+
+    console.info("File %s created under %s", createdFile.getName(), folder.getName());
   });
 }
 
 function cloneAttachmentsToDrive(label, folderId){
   var statementLabel = GmailApp.getUserLabelByName(label);
   var attachmentProcessedLabel = GmailApp.getUserLabelByName(ATTACHMENT_PROCESSED_LABEL);
-  
+
   if(attachmentProcessedLabel == undefined){
     attachmentProcessedLabel = GmailApp.createLabel(ATTACHMENT_PROCESSED_LABEL);
   }
-  
+
   var threadIndex = 0;
   var threadRange = 100;
-  
+
   var cloneStartTimeDate = new Date();
-  
+
   while(true){
     if(isTimeUp_(cloneStartTimeDate)){
-      Logger.log("Over Time. Need some break");
+      console.error("Over Time. Need some break");
       break;
     }
-    
+
     var threads = statementLabel.getThreads(threadIndex, threadRange);
-    
+
     if(threads.length > 0){
       var unProcessedThreads = getUnprocessedThreads(threads);
       var msgs = GmailApp.getMessagesForThreads(unProcessedThreads);
-      
+
       for (var i = 0 ; i < msgs.length; i++) {
         for (var j = 0; j < msgs[i].length; j++) {
-          var threadMessage = msgs[i][j]; 
+          var threadMessage = msgs[i][j];
           var attachments = threadMessage.getAttachments();
-          
+
           cloneAttachmentsToFolder(threadMessage, attachments, folderId);
           threadMessage.getThread().addLabel(attachmentProcessedLabel);
         }
       }
-    }
-    else {
+    } else {
       break;
     }
-    
+
     threadIndex = threadIndex + threadRange;
   }
 }
--- Code.js
+++ Code.js
@@ -1,15 +1,19 @@
 function initializeAttachmentProcessor() {
   var cloneStartTimeDate = new Date();
-    
+
+  console.info("Attachments Processing Started On %s", new Date());
+
   ATTACHMENT_MAILS_CONFIG.forEach(function(attachmentMailConfig){
     if(isTimeUp_(cloneStartTimeDate)){
-      Logger.log("Over Time. Need some break");
+      console.error("Over Time. Need some break");
       return;
     }
-    
+
     var label = attachmentMailConfig.label;
     var folderId = attachmentMailConfig.folderId;
-    
+
     cloneAttachmentsToDrive(label, folderId);
   });
+
+  console.info("Attachments Processing Ended On %s", new Date());
 }
